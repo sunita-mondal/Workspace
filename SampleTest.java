afbb1e2103226a41fad518d8215acad24b941da2
sunita-mondal <129615397+sunita-mondal@users.noreply.github.com>
2023-04-02 17:10:46 +0530

    Create SampleTest.java

---
+++ SampleTest.java
@@ -0,0 +1,11 @@
+public class SampleTest {
+  public static void main(String[] args) {
+    int x=5;
+    while (x < 15) {
+      System.out.println(x);
+      x++;
+      }
+  System.out.println("the sun");    
+    
+  }
+}
